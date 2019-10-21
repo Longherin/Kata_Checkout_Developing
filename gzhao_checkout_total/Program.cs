05b725b3028ecb1b2645431f77307ec7c23b20ae
Longherin <Longherin@gmail.com>
2019-10-21 10:45:35 -0400

    Initial Project Build

---
+++ gzhao_checkout_total/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gzhao_checkout_total
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
